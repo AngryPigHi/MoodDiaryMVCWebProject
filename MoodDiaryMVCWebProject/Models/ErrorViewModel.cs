3440427d63621471ade7201d4f884a9f99cd3c14
Blithe_Zhu <49220637+AngryPigHi@users.noreply.github.com>
2023-08-10 11:15:53 +0800

    添加项目文件。

---
+++ MoodDiaryMVCWebProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MoodDiaryMVCWebProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
